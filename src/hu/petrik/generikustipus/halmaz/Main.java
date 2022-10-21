aa52c460f376ffd2b6af395dbc35fa3b825e82f5
Budaházi Bence <budahazi.bence@petrik.hu>
2022-10-21 16:10:23 +0200

    Initial commit

---
+++ src/hu/petrik/generikustipus/halmaz/Main.java
@@ -0,0 +1,16 @@
+package hu.petrik.generikustipus.halmaz;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            BuszAtszallas atszallas_102_105 = new BuszAtszallas("files/102.txt", "files/105.txt");
+            Halmaz<String> kozosMegallok_102_105 = atszallas_102_105.kozosMegallok();
+            System.out.printf("%d megálló van, ahol a 102-es buszról a 105-ös buszra átszálhatunk. Ezek a megállók: %s",
+                    kozosMegallok_102_105.getSize(), kozosMegallok_102_105);
+        } catch (FileNotFoundException e) {
+            System.out.println("Nem találhatóak a forrásfájlok");
+        }
+    }
+}
